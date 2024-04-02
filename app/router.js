9b86e14fb20b8f7ae4f4936e757074c971befd0b
Ali Hejazi <ali.hejazi@intercom.io>
2024-04-02 16:11:37 +0100

    Added contact page

--- app/router.js
+++ app/router.js
@@ -8,4 +8,10 @@ export default class Router extends EmberRouter {
 
 Router.map(function () {
   this.route('about');
+  this.route('contact', {
+    // reason we override the path is that the tutorial assumes imaginary scenario that company has a legacy
+    // webpage on the /contact route which we cannot change, so we need a new route (called /getting-in-touch)
+    // but we still want to internally refer to this page as the 'contact' page
+    path: '/getting-in-touch',
+  });
 });
